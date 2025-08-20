bc8c4ef86ff610512cb58956029bbb99f7ec8a94
Jesus Palet <104393790+JeaLPaHu@users.noreply.github.com>
2025-08-20 14:35:25 -0600

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigoSecreto = [];
+
+function agregarAmigoSecreto() {
+    let nombre = document.getElementById("nombre").value;
+    if (nombre) {
+        amigoSecreto.push(nombre);
+        document.getElementById("nombre").value = "";
+        mostrarAmigos();
+    } else {
+        alert("Por favor, ingresa un nombre.");
+    } else {
+        nombre.push(nombre);
+        document.querySelector("#nombre").value = "";
+        mostrarAmigos();
+    }
+}
+
